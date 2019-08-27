b0e6f2178c35b373cf360d66eca98e6f6fd77ab4
Angel-smith974 <elfedelanuit974@hotmail.fr>
2019-08-27 13:23:58 +0400

    commit de 13h30

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*
+const prenom = prompt("Entrez votre prénom :");
+alert(`Bonjour, ${prenom}`);
+*/
+
+/*
+const saisie = prompt("Entez un nombre :");
+const nb = Number (saisie);
+alert (`votre nombre : \n ${nb}`);
+*/
+
+/*
+const rayon = prompt("Entrez le rayon pour calculé le périmètre du disque");
+const pi = 3.14 ;
+const perimetre = 2 * pi * rayon;
+alert(`Voici le résultat : ${perimetre}`);
+*/
