db9d33374706a735548bac9c29a2ef5e8fa61fb2
Angel-smith974 <elfedelanuit974@hotmail.fr>
2019-08-28 08:21:20 +0400

    commit du matin

--- js/script.js
+++ js/script.js
@@ -15,3 +15,54 @@ const pi = 3.14 ;
 const perimetre = 2 * pi * rayon;
 alert(`Voici le résultat : ${perimetre}`);
 */
+
+//Tuto
+/* 
+const nombre = Number(prompt("Entrez un nombre entre 1 et 100 :"));
+
+if (nombre > 0) {
+    alert(`${nombre} est positif`);
+}
+
+else {
+ if (nombre < 0) {
+    alert(`${nombre} est négatif`);
+    }
+    else {
+        alert(`${nombre} est nul`);
+    }
+}
+// "&&" ça veux dire "et"
+if ((nombre >= 0)&&(nombre <=100)){
+    console.log(nombre + ' est compris entre 0 et 100');
+}
+// "||" ça veux dire "ou"
+if ((nombre <0) || (nombre > 100)) {
+    alert(`${nombre} est en dehors de l'intervalle [0, 100]`)    
+}
+*/
+
+//Mon teste perso :
+const nombre = Number(prompt("Entrez un nombre entre -100 et 100 :"));
+
+
+// "&&" ça veux dire "et"
+if ((nombre >= -100)&&(nombre <=100)){
+    console.log(nombre + ' est compris entre -100 et 100');
+    if (nombre > 0) {
+        alert(`${nombre} est positif`);
+    }
+    
+    else {
+     if (nombre < 0) {
+        alert(`${nombre} est négatif`);
+        }
+        else {
+            alert(`${nombre} est nul`);
+        }
+    }
+}
+// "||" ça veux dire "ou"
+if ((nombre <-100) || (nombre > 100)) {
+    alert(`${nombre} est en dehors de l'intervalle proposé ! Espèce de NOOB !`)    
+}
